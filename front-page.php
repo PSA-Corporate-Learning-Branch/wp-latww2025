8ac628444fa16004d7ade958217cb1b4d7b29afb
Nori Sinclair <115030892+bcgov-noris@users.noreply.github.com>
2025-12-17 13:57:40 -0800

    Update front-page.php
    remove 2025 info and update postponement messaging. Content to come later

--- front-page.php
+++ front-page.php
@@ -2,36 +2,27 @@
 <div id="content">
     <!-- Banner Section -->
     <div class="banner-container">
-        <div class="banner-image">
-            <h1 class="visually-hidden">Your Career Journey: Upskilling at Every Stage</h1>
+        <div class="bg-blue">
+            <h1 class="visually-hidden">Your Time to Learn</h1>
         </div>
     </div>
-    <h2 class="mb-0 p-3 text-center text-white" style="background-color: #2f3442;">Learn @ Work Week: February 2026</h2>
+    <h2 class="mb-0 p-3 text-center text-white" style="background-color: #2f3442;">Learn @ Work Week: February 2 to 6, 2026</h2>
     <!-- Main Content Section -->
     <div class="container-lg px-4 px-md-5">
         <div class="bg-secondary-subtle card my-3 rounded shadow-sm">
                     <div class="card-body fs-6">
-                        <h3 class="card-title fs-4">Learn @ Work Week Postponement</h3>
-                        <p class="card-text">L@WW, originally scheduled for October 27 to 31, has been postponed to February 2026. Exact dates will be confirmed shortly.</p>
-                        <p class="card-text">We’ve seen strong interest in L@WW, and we remain committed to delivering a meaningful and accessible learning experience for BC Public Service employees. We’re taking this time to adjust the timing and approach to ensure the event meets your needs. If you were previously registered, you’ll be given the first opportunity to re-register once the event is rescheduled.</p>
-                        <p class="card-text">We encourage you to continue exploring opportunities to grow your skills and reflect on your career journey in ways that work for you at this time. Thank you for your understanding. </p>
-                        <p class="card-text">Watch for communications with updated dates and details. We look forward to seeing you at L@WW in February! </p>
-                     </div>
+                        <h3 class="card-title fs-4">Learn @ Work Week: February 2 to 6, 2026</h3>
+                        <p class="card-text">Learn @ Work Week (L@WW) returns February 2 to 6, 2026 with a special edition, L@WW: Your Time to Learn.</p>
+                        <p class="card-text">More details, including playlists, live session schedule and registration will be shared soon.</p>
+                        <p class="card-text"><strong>Looking ahead:</strong> We’ve seen strong interest in the sessions planned for fall 2025. These sessions will return in fall 2026, giving you another opportunity to grow your skills and reflect on your career journey.</p>
+                    </div>
                 </div>
         <div class="row my-md-5">
             <!-- Main Content -->
             <div class="col-lg-8 mb-4 mb-lg-0">
                 <h2>About Learn @ Work Week</h2>
                 <p>Learn @ Work Week (L@WW) is a fully virtual event that aims to reach all BC Public Service employees. Learning activities raise awareness and encourage conversations about key topics relevant to everyone. </p>
-                <h3>Theme: Your Career Journey: Upskilling at Every Stage</h3>
-                <p>This theme focuses on how the BC Public Service can support employee learning goals throughout their entire career. It highlights the value of continuous learning and shows the many learning and development opportunities available to employees.</p>
-                <p>Each session focuses on a different topic around career growth, from new employees through mid-career development, leadership opportunities and more. Join us to:</p>
-                <ul>
-                    <li>Explore tools, resources and strategies that support career growth within the BC Public Service
-                    </li>
-                    <li>Identify how skills, concepts, or approaches introduced during the sessions can support your development or career progression</li>
-                </ul>
-                <!-- Event Description Section -->
+               
             </div>
             <!-- Sidebar Section -->
             <div class="col-lg-4">
@@ -43,67 +34,8 @@
                         <p class="card-text">If you need any accommodations to participate in L@WW, please <a href="https://sfs7.gov.bc.ca/affwebservices/public/saml2sso?SPID=urn:ca:bc:gov:customerportal:prod">submit an AskMyHR service request</a> using the category "Corporate Learning".</p>
                     </div>
                 </div>
-                <div class="card shadow-sm rounded">
-                    <div class="card-body fs-6">
-                        <h3 class="card-title fs-4">Daily Newsletter</h3>
-                        <p class="card-text">Receive daily updates highlighting the L@WW sessions of the day, along with relevant resources to help reinforce your learning or catch up on sessions you may have missed.</p>
-                        <a href="https://submit.digital.gov.bc.ca/app/form/submit?f=fd03b54b-84aa-4a05-b5ff-c5536b733f57" class="btn btn-primary">Subscribe to the newsletter</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
-    <!-- Schedule Section -->
-    <div class="bg-dark-subtle">
-        <div class="container-xl p-4 p-md-5">
-            <h3 class="mb-2">Learn @ Work Week 2025 Schedule</h3>
-            <p>Select a session to get more details about the event and register. </p>
-            <p><strong>All times are shown for the Pacific Time Zone. </strong></p>
-            <?php get_template_part('template-parts/schedule') ?>
-        </div>
-    </div>
-    <!-- Keynote promo -->
-    <div class="container-lg p-4 p-md-5">
-        <div class="row">
-            <div class="col-lg-8">
-                <?php $keynote = get_page(293) ?>
-                <?php
-                $eventDateTime = get_post_meta($keynote->ID, 'eventDateTime', TRUE);
-                $iso8601_datetime = $eventDateTime;
-                // Create a new DateTime object from the ISO8601 string
-                $datetime = new DateTime($iso8601_datetime, new DateTimeZone('UTC'));
-                // Convert to the "America/Vancouver" timezone
-                $datetime->setTimezone(new DateTimeZone('America/Vancouver'));
-                // Get the current time in the "America/Vancouver" timezone
-                $current_time = new DateTime('now', new DateTimeZone('America/Vancouver'));
-                // Convert both DateTime objects to timestamps
-                $datetime_timestamp = $datetime->getTimestamp();
-                $current_timestamp = $current_time->getTimestamp();
-                ?>
-                <h3><a class="text-decoration-none" href="/latww2025/monday/<?= $keynote->post_name ?>"><?= $keynote->post_title ?></a></h3>
-                <p><?= $keynote->shortDesc ?></p>
-                <?php if ($current_timestamp < $datetime_timestamp): ?>
-                    <?php if (!empty($keynote->registrationLink) && empty($keynote->sessionFull)): ?>
-                        <?php $tt = $keynote->post_title ?>
-                        <a href="<?= $keynote->registrationLink ?>" class="btn btn-primary">Register: <?= mb_strimwidth($tt, 0, 45, '...') ?></a>
-                    <?php else: ?>
-                        <?php if (!empty($keynote->sessionFull)): ?>
-                            <div class="alert alert-secondary">This session is now full!</div>
-                        <?php else: ?>
-                            <div class="alert alert-secondary">Not open for registration yet.</div>
-                        <?php endif ?>
-                    <?php endif ?>
-                <?php else: ?>
-                    <div class="alert alert-secondary">No longer open for registration.</div>
-                <?php endif ?>
-            </div>
-            <div class="col-lg-4">
-                <div class="d-flex justify-content-center" style="height: 12rem;">
-                    <img src="https://learn.bcpublicservice.gov.bc.ca/latww/latww2025/img/latww-swoop.svg">
-                </div>
-            </div>
-        </div>
-    </div>
-
 
     <?php get_footer() ?>
